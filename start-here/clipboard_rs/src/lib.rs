02881797764a7ebb17294915382179817f55ecd9
Bret Barkelew <brbarkel@microsoft.com>
2020-02-10 23:27:08 -0800

    Rust is now getting text out of the Clipboard.

--- start-here/clipboard_rs/src/lib.rs
+++ start-here/clipboard_rs/src/lib.rs
@@ -7,6 +7,17 @@ mod bindings;
 
 // use bindings::*;
 
+use winapi::um::{winbase, winuser};
+use winapi::shared::{ntdef, minwindef};
+
+unsafe fn str16_len(str16_ptr: *const u16) -> usize {
+    let mut len: usize = 0;
+    while (str16_ptr.add(len).read() != 0) {
+        len += 1;
+    }
+    len
+}
+
 struct ClipboardGuard;
 
 impl ClipboardGuard {
@@ -18,13 +29,33 @@ impl ClipboardGuard {
 
         match call_status {
             false => Err(unsafe{ bindings::GetLastError() }),
-            true => Ok(ClipboardGuard{})
+            true => Ok(Self{})
+        }
+    }
+
+    pub fn get_text(&self) -> Result<String, u32> {
+        match unsafe { winuser::GetClipboardData(winuser::CF_UNICODETEXT) } {
+            ntdef::NULL => Err(unsafe{ bindings::GetLastError() }),
+            data_handle => {
+                match unsafe { winbase::GlobalLock(data_handle) } {
+                    ntdef::NULL => Err(0),  // TODO: What should actually happen here?
+                    clipboard_os_text => {
+                        // Okay... so...
+                        // clipboard_os_text is a pointer to a u16 string.
+                        // We need to figure out how long it is if we want a String.
+                        let str_len = unsafe { str16_len(clipboard_os_text as *const u16) };
+                        let text_slice = unsafe { std::slice::from_raw_parts(clipboard_os_text as *const u16, str_len) };
+                        Ok(String::from_utf16_lossy(&text_slice))
+                    }
+                }
+            }
         }
     }
 }
 
 impl Drop for ClipboardGuard {
     fn drop(&mut self) {
+        // println!("Closing Clipboard on Drop");
         unsafe { bindings::CloseClipboard() };
     }
 }
@@ -34,5 +65,7 @@ impl Drop for ClipboardGuard {
 pub extern "C" fn print_clipboard_file(filename: *const u16, len: usize) {
     let name_slice = unsafe { std::slice::from_raw_parts(filename, len) };
     let file_name = String::from_utf16_lossy(&name_slice);
-    println!("{}", file_name);
+
+    let my_clipboard = ClipboardGuard::new(None).unwrap();
+    println!("{}", my_clipboard.get_text().unwrap());
 }
